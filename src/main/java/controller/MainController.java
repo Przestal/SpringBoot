bea2bed69a815b3473fed36fe71553343bf04f28
przestal <przemyslawg91@gmail.com>
2018-02-28 23:22:18 +0100

    added main/controller structure

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,11 +1,15 @@
 package controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
 public class MainController {
-    public static void main(String[] args) {
-        SpringApplication.run(MainController.class, args);
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World";
     }
+
+
 }
